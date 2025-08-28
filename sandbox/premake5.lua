b39b9fff4011454e5ff5e9faaf3cbf9590899f82
Nessbe <felix.chaupite@gmail.com>
2025-08-28 19:19:57 +0200

    Configured Citadel exceptions

--- sandbox/premake5.lua
+++ sandbox/premake5.lua
@@ -63,6 +63,8 @@ project "sandbox"
 			"SANDBOX_DEBUG",
 			"CITADEL_ENABLE_DEBUGBREAK",
 			"CITADEL_ENABLE_ASSERTIONS",
+			"CITADEL_ENABLE_THROWING",
+			"CITADEL_ENABLE_EXCEPTIONS",
 		}
 		symbols "On"
 		runtime "Release"
